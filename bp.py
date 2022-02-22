680171ea4a7de633a1a9cdca16849f5603152e42
Lenny-98 <95129719+Lenny-98@users.noreply.github.com>
2022-02-22 18:14:56 +0100

    Update bp.py

--- bp.py
+++ bp.py
@@ -96,57 +96,7 @@ fig.tight_layout()
 st.pyplot(fig)
 
 
-# In[6]:
 
-
-
-
-fig, ax = plt.subplots()
-
-#Set colors
-data_color = ['#000099', '#009BD2', '#4BD0FF', '#87BC49', '#F5C500', '#FFE060']
-
-# Save the chart so we can loop through the bars below.
-bars = ax.bar(
-    x=df['Unternehmen'],
-    height=df['Image'],
-    color = data_color
-)
-
-# Axis formatting.
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.spines['left'].set_visible(False)
-ax.spines['bottom'].set_color('#DDDDDD')
-ax.tick_params(bottom=False, left=False)
-ax.set_axisbelow(True)
-ax.yaxis.grid(True, color='#EEEEEE')
-ax.xaxis.grid(False)
-
-# Add text annotations to the top of the bars.
-bar_color = bars[0].get_facecolor()
-for bar in bars:
-    ax.text(
-     bar.get_x() + bar.get_width() / 2,
-     bar.get_height() + 0.2,
-     round(bar.get_height(), 1),
-     horizontalalignment='center',
-     color=bar_color,
-     weight='bold'
- )
-
-# Add labels and a title.
-ax.set_xlabel('Xlabel', labelpad=15, color='#333333')
-ax.set_ylabel('Ylabel', labelpad=15, color='#333333')
-ax.set_title('Company Comparison', pad=15, color='#333333',
-             weight='bold')
-
-fig.tight_layout()
-
-st.pyplot()
-
-
-# In[ ]:
 
 
 
